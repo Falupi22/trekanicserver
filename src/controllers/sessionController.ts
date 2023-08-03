2c2181436306e3a6a4a698010283d9ec7e00bff7
Tal Frumkin <talfru22@gmail.com>
2023-08-03 17:22:15 +0300

    NO-ISSUE: Added logout, fixed bugs.

--- src/controllers/sessionController.ts
+++ src/controllers/sessionController.ts
@@ -11,26 +11,50 @@ export const login = asyncHandler(async (req, res, next) => {
         password: req.body.password
     });
 
-    if (await User.findOne({ username: user.username, password: user.password })) {
-        await new Promise((resolve, reject) => req.login(user, async function (error) {
-            if (error) {
-                console.log(error);
-            } else {
-                await new Promise((resolve , reject) => {passport.authenticate("local", function (error, user, info) {
-                    if (error) {
-                        console.log(error);
-                        statusCode = HttpStatus.UNAUTHORIZED;
-                    }
-                    else {
-                        statusCode = HttpStatus.OK;
-                    }
+    if (!req.isAuthenticated()) {
+        if (await User.findOne({ username: user.username, password: user.password })) {
+            await new Promise((resolve, reject) => req.login(user, async function (error) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    await new Promise((resolve, reject) => {
+                        passport.authenticate("local", function (error, user, info) {
+                            if (error) {
+                                console.log(error);
+                                statusCode = HttpStatus.UNAUTHORIZED;
+                            }
+                            else {
+                                statusCode = HttpStatus.OK;
+                            }
+
+                            resolve(null);
+                        })(req, res, next);
+                    });
 
                     resolve(null);
-                })(req, res, next);
-            });
+                }
+            }));
+        }
+    }
+    else {
+        statusCode = HttpStatus.FORBIDDEN;
+    }
 
-            resolve(null);
-        }}));
+    res.status(statusCode);
+    res.send();
+});
+
+export const logout = asyncHandler(async (req, res, next) => {
+    let statusCode: number = HttpStatus.FORBIDDEN;
+
+    if (req.isAuthenticated()) {
+        await new Promise((resolve, reject) => {
+            req.logout(null, () => {
+                statusCode = HttpStatus.OK;
+
+                resolve(null);
+            });
+        });
     }
 
     res.status(statusCode);
