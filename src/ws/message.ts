e9c4cb7269f0dab04ce137344a4675156fe36550
Zbreakmath <raz.frumkin@gmail.com>
2023-09-04 23:59:40 +0300

    NO-ISSUE: Revert

--- src/ws/message.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-import { messageSchema } from "./schemas";
-import { createEvent } from "./socket";
-
-export const sendMessage = createEvent("message:send", messageSchema
-, async (socket, { message, to }) => {
-    // Insert your logic here
-  });
--- src/ws/schemas.ts
+++
@@ -1,6 +0,0 @@
-import Joi, { ObjectSchema } from "joi";
-
-export const messageSchema: ObjectSchema = Joi.object().keys({
-    message: Joi.string().required().description("The content of the message"),
-    to: Joi.string().optional().description("The name of the user to send the message")
-  });
